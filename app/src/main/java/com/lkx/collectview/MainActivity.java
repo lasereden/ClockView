46733b412faead9023e87587913ca9c3f494f0a0
Taiq Wu <taiq_wu@163.com>
2024-10-30 15:33:42 +0800

    隐藏标题栏

--- app/src/main/java/com/lkx/collectview/MainActivity.java
+++ app/src/main/java/com/lkx/collectview/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // 保持屏幕常亮，不锁屏
         mHandler.sendEmptyMessage(0);
     }

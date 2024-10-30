3af8e27de77b8694e9d3ba9462377c03ad1532dc
Taiq Wu <taiq_wu@163.com>
2024-10-30 13:51:02 +0800

    横竖屏切换

--- app/src/main/java/com/lkx/collectview/MainActivity.java
+++ app/src/main/java/com/lkx/collectview/MainActivity.java
@@ -1,10 +1,12 @@
 package com.lkx.collectview;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import java.util.Calendar;
 
@@ -20,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
                 lastSecond = second;
                 int minute = calendar.get(Calendar.MINUTE);
                 int hour = calendar.get(Calendar.HOUR_OF_DAY);
-                getSupportActionBar().setTitle(String.format("%02d:%02d:%02d", hour, minute, second));
+                TextView textViewTime = findViewById(R.id.textViewTime);
+                textViewTime.setText(String.format("%02d:%02d:%02d", hour, minute, second));
             }
             //250ms后再刷新一次页面
             sendEmptyMessageDelayed(0, 250);
@@ -34,4 +37,11 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // 保持屏幕常亮，不锁屏
         mHandler.sendEmptyMessage(0);
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setContentView(R.layout.activity_main);
+    }
+
 }

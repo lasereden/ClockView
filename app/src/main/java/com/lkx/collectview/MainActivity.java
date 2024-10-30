d62771a3e1e6e7919cece3d7a7792e8421cefa65
Taiq Wu <taiq_wu@163.com>
2024-10-30 19:25:16 +0800

    钟面直径自适应屏幕

--- app/src/main/java/com/lkx/collectview/MainActivity.java
+++ app/src/main/java/com/lkx/collectview/MainActivity.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    final static private String TAG = "MainActivity";
 
+    private int diameterPixels;
     private int lastSecond = -1;
+
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -37,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide(); // 隐藏标题栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // 隐藏状态栏
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // 保持屏幕常亮，不锁屏
+
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int widthPixels = dm.widthPixels;
+        int heightPixels = dm.heightPixels;
+        diameterPixels = widthPixels < heightPixels ? widthPixels : heightPixels;
+        Log.d(TAG, String.format("diameter %dpx %ddp", diameterPixels, DensityUtil.px2dip(this, diameterPixels)));
+        fixOrientation(getResources().getConfiguration().orientation);
+
         mHandler.sendEmptyMessage(0);
     }
 
@@ -44,6 +58,19 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         setContentView(R.layout.activity_main);
+        fixOrientation(newConfig.orientation);
     }
 
+    private void fixOrientation(int orientation) {
+        ClockView clockView = findViewById(R.id.clockView);
+        ViewGroup.LayoutParams params = clockView.getLayoutParams();
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) { // 横屏处理
+            Log.d(TAG, "横屏");
+            params.height = diameterPixels;
+        } else if (orientation == Configuration.ORIENTATION_PORTRAIT) { // 竖屏处理
+            Log.d(TAG, "竖屏");
+            params.width = diameterPixels;
+        }
+        clockView.setLayoutParams(params);
+    }
 }
